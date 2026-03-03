5f7b70a364cf76de25228cee02aac412533fa2af
mtekinn <tekinmdev@gmail.com>
2026-03-03 18:09:44 +0300

    Add JWT auth and security config

--- fintrack/src/main/java/com/example/fintrack/FintrackApplication.java
+++ fintrack/src/main/java/com/example/fintrack/FintrackApplication.java
@@ -3,7 +3,7 @@ package com.example.fintrack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@SpringBootApplication()
 public class FintrackApplication {
     public static void main(String[] args) {
         SpringApplication.run(FintrackApplication.class, args);
